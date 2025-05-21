e400d6b88d7399c6a5260a0f845ca26054a76628
Minoru Osuka <minoru.osuka@gmail.com>
2025-05-21 22:17:50 +0900

    Add CopyWevpackPlugin

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -1,21 +1,29 @@
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js', // エントリーポイント
+    entry: './src/index.js',
     output: {
-        filename: 'bundle.js', // 出力ファイル名
-        path: path.resolve(__dirname, 'dist'), // 出力先をexample内のdistに設定
-        publicPath: '/example/', // サーバーでのパスを指定
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/example/',
     },
     mode: 'development',
     experiments: {
-        asyncWebAssembly: true, // WASMの非同期読み込みを有効化
+        asyncWebAssembly: true,
     },
     devServer: {
         static: {
-            directory: path.resolve(__dirname, 'dist'), // 静的ファイルの提供元
+            directory: path.resolve(__dirname, 'dist'),
         },
-        open: true, // サーバー起動時にブラウザを自動で開く
-        port: 8080, // ポート番号
+        open: true,
+        port: 8080,
     },
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: path.resolve(__dirname, '../pkg/*.wasm'), to: '[name][ext]' }
+            ]
+        })
+    ]
 };
