2908b7ac5376ca2cb35fde6b6955a9cf62863d7c
misogihagi <47350059+misogihagi@users.noreply.github.com>
2025-05-23 00:58:01 +0900

    add sample data

---
+++ example/performance/src/lib/constants/index.ts
@@ -0,0 +1,23 @@
+import text from './text.json';
+
+const CONSTANTS = {
+	texts: [
+		{
+			id: 0,
+			display: 'すもももももももものうち（12文字）',
+			text: text[0]
+		},
+		{
+			id: 1,
+			display: 'ごんぎつね（約5千文字）',
+			text: text[1]
+		},
+		{
+			id: 2,
+			display: 'ドグラ・マグラ（約4万文字）',
+			text: text[2]
+		}
+	]
+};
+
+export default CONSTANTS;
